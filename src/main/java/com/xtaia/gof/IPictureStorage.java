765147878a56b4a5a0f67fc1b16314c586bcf506
xtaia <307205761@qq.com>
2019-12-19 13:42:34 +0800

    feat：抽象、封装、继承

---
+++ src/main/java/com/xtaia/gof/IPictureStorage.java
@@ -0,0 +1,8 @@
+package com.xtaia.gof;
+
+public interface IPictureStorage {
+    void savePicture(Picture picture);
+    Image getPicture(String pictureId);
+    void deletePicture(String pictureId);
+    void modifyMetaInfo(String pictureId, PictureMetaInfo metaInfo);
+}
---
+++ src/main/java/com/xtaia/gof/PictureStorage.java
@@ -0,0 +1,24 @@
+package com.xtaia.gof;
+
+public class PictureStorage implements IPictureStorage {
+
+    @java.lang.Override
+    public void savePicture(Picture picture) {
+
+    }
+
+    @java.lang.Override
+    public Image getPicture(String pictureId) {
+        return null;
+    }
+
+    @java.lang.Override
+    public void deletePicture(String pictureId) {
+
+    }
+
+    @java.lang.Override
+    public void modifyMetaInfo(String pictureId, PictureMetaInfo metaInfo) {
+
+    }
+}
